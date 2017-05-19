e60bfe1609764029f05650d90d520a66b2cda24d
AmanDhembla <amandhembla@gmail.com>
2017-05-20 01:44:26 +0530

    name Ascii value Calculator implemented

--- javascript/app.js
+++ javascript/app.js
@@ -4,6 +4,15 @@
   angular.module('myApp',[])
 
   .controller('myController',function($scope){
-      $scope.name="aman";
+      $scope.name="";
+      $scope.value=0;
+
+      $scope.calculateValue=function(){
+        var fvalue=0;
+        for(var i=0;i<$scope.name.length;i++){
+          fvalue=fvalue+$scope.name.charCodeAt(i);
+        }
+        $scope.value=fvalue;
+      };
   });
 })();
