6761011218787aae3711a126a9405f05c24fec0e
AmanDhembla <amandhembla@gmail.com>
2017-05-20 19:57:47 +0530

    page created to add shopping items to the list

---
+++ javascript/app1.js
@@ -0,0 +1,32 @@
+(function(){
+  'use strict';
+
+  var shoppingList1=["colgate","brush","sugar","salt","chilli powder","honey"];
+
+  var shoppingList2=[
+    {
+      name:"ladoo",
+      quantity:50
+    },
+    {
+      name:"barfi",
+      quantity:100
+    }
+];
+  angular.module('to_do_list',[])
+
+  .controller('listController',listController);
+
+  listController.$inject=['$scope'];
+  function listController($scope){
+    $scope.shoppingList1=shoppingList1;
+    $scope.shoppingList2=shoppingList2;
+    $scope.save_to_list=function(){
+      var item={
+        name:$scope.item_name,
+        quantity:$scope.item_quantity
+      };
+      shoppingList2.push(item);
+    }
+  }
+})();
